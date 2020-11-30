156da3329db56d12d9119394fd286ab88467f6cd
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-11-30 10:13:55 -0300

    Services Section

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import AboutSecion from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 // import { Container } from './styles';
 
 const AboutUs: React.FC = () => {
-  return <AboutSecion />;
+  return (
+    <>
+      <AboutSecion />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
